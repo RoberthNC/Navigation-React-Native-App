60845d03035c5bb6b0c66203cb9e6fe717d691f9
Roberto Núñez <robertonc1599@gmail.com>
2024-05-26 19:47:05 -0500

    Stack navigator configuration and styling, Custom Primary Button Component

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,22 @@
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {globalStyles} from '../../theme/theme';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={globalStyles.container}>
+      <PrimaryButton
+        label="Products"
+        onPress={() => navigation.navigate('Products' as never)}
+      />
+
+      <PrimaryButton
+        label="Settings"
+        onPress={() => navigation.navigate('Settings' as never)}
+      />
     </View>
   );
 };
